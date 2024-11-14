c2d0e4fb6d34a1fda62f85dbab1e34f5d92fe9c4
bikooju <jhjk1234@gmail.com>
2024-11-14 19:19:35 +0900

    week6

--- src/main/java/umc/spring/repository/StoreRepository/StoreRepositoryImpl.java
+++ src/main/java/umc/spring/repository/StoreRepository/StoreRepositoryImpl.java
@@ -4,6 +4,7 @@ import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import umc.spring.domain.QStore;
 import umc.spring.domain.Store;
 
 import java.util.List;
@@ -12,7 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class  StoreRepositoryImpl implements StoreRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
-    private final QStore store = Qstore.store;
+    private final QStore store = QStore.store;
 
     @Override
     public List<Store> dynamicQueryWithBooleanBuilder(String name, Float score) {
